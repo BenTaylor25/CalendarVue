6769191fe58563bb332ceff76c947f872bae229a
Ben Taylor <ben.taylor2573@gmail.com>
2024-07-12 22:25:51 +0100

    added template for month view

--- calendar-vue/src/components/calendar/CalendarHeader.vue
+++ calendar-vue/src/components/calendar/CalendarHeader.vue
@@ -3,7 +3,15 @@
 
 <template>
   <div id="calendar-header">
-    <div id="month-control"></div>
+
+    <div id="month-control">
+      <p>month</p>
+    </div>
+
+    <div id="time-bar">
+
+    </div>
+
   </div>
 </template>
 
@@ -11,5 +19,20 @@
 #calendar-header {
   border: 1px solid blue;
   height: 12.5%;
+
+  #month-control {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: min(10%, 10rem);
+
+    background-color: purple; // temp
+
+    p {
+      color: black;
+    }
+
+  }
 }
 </style>
