d9be1c674ab2d3e22978b893302c82bea9cc133d
Ben Taylor <ben.taylor2573@gmail.com>
2024-07-13 17:44:29 +0100

    added Zoom button to Header

--- calendar-vue/src/components/PageHeader.vue
+++ calendar-vue/src/components/PageHeader.vue
@@ -1,13 +1,43 @@
 <script setup lang="ts">
 
+function openZoomSettingModal() {
+
+}
+
 </script>
 
 <template>
   <div id="header">
-    <p>header</p>
+
+    <div id="settings">
+
+      <div id="zoom-setting" @click="openZoomSettingModal">
+        <p>Zoom</p>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <style lang="scss">
+
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+
+  #settings {
+    margin-right: 1rem;
+
+    * {
+      height: 50%;
+      background: blue;
+      padding: 0.1rem 0.3rem;
+      cursor: pointer;
+    }
+
+  }
+}
 
 </style>
