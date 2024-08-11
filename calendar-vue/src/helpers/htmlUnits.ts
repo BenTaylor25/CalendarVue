c24108fc16d5d8e0a05d304f3b70a7f6900e8072
Ben Taylor <ben.taylor2573@gmail.com>
2024-08-11 11:37:53 +0100

    added error handling to htmlUnits helper

--- calendar-vue/src/helpers/htmlUnits.ts
+++ calendar-vue/src/helpers/htmlUnits.ts
@@ -4,5 +4,25 @@
  * - `const px = rem * remToPx();`
  */
 export function remToPx(): number {
-    return parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+    //#region Error Handling
+    if (typeof getComputedStyle === "undefined") {
+        console.error("Could not find function 'getComputedStyle'. Browser likely out of date.");
+    }
+
+    if (document?.documentElement === undefined) {
+        console.error("`document` or 'document.documentElement' was undefined.");
+    }
+    //#endregion
+
+    const fontSizeString = getComputedStyle(document.documentElement).fontSize;
+    const fontSize = parseFloat(fontSizeString);
+
+    //#region Error Hanlding
+    if (isNaN(fontSize)) {
+        console.error(`Rem font size '${fontSizeString}' is not a number.`);
+    }
+    //#endregion
+
+    return fontSize;
 }
