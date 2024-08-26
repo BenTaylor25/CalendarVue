75eff67837553ebdcf829b99df353c9df2ad8739
Ben Taylor <ben.taylor2573@gmail.com>
2024-08-26 17:13:41 +0100

    seperated out CalendarMonthDay

---
+++ calendar-vue/src/components/calendar/month-view/CalendarMonthDay.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="month-day" />
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped lang="scss">
+.month-day {
+  width: calc(100% / 7);
+  height: 100%;
+  border: 1px dotted gray;
+
+  &:hover {
+    background-color: green;
+  }
+}
+</style>
--- calendar-vue/src/components/calendar/month-view/CalendarMonthWeekRow.vue
+++ calendar-vue/src/components/calendar/month-view/CalendarMonthWeekRow.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-
+import CalendarMonthDay from './CalendarMonthDay.vue';
 </script>
 
 <template>
   <div class="month-week-row">
-    <div class="month-day mon" />
-    <div class="month-day tue" />
-    <div class="month-day wed" />
-    <div class="month-day thu" />
-    <div class="month-day fri" />
-    <div class="month-day sat" />
-    <div class="month-day sun" />
+    <calendar-month-day class="mon" />
+    <calendar-month-day class="tue" />
+    <calendar-month-day class="wed" />
+    <calendar-month-day class="thu" />
+    <calendar-month-day class="fri" />
+    <calendar-month-day class="sat" />
+    <calendar-month-day class="sun" />
   </div>
 </template>
 
@@ -22,16 +22,6 @@
   display: flex;
   width: 100%;
   height: 15%;
-
-  .month-day {
-    width: calc(100% / 7);
-    height: 100%;
-    border: 1px solid gray;
-
-    &:hover {
-      background-color: green;
-    }
-  }
 
   .fri {
     margin-right: 10%;
