2873f1b6b596d57210f5869f30bcb4c4199d5bce
Ben Taylor <ben.taylor2573@gmail.com>
2024-09-08 11:58:23 +0100

    read events from the server

---
+++ calendar-vue/src/api/getAllEvents.ts
@@ -0,0 +1,37 @@
+import { API_CALENDAR_EVENT_ROUTE } from "../constants/api.ts";
+import { CalendarEventModel } from "../models/CalendarEventModel.ts";
+
+interface ApiEventResponse {
+    id: string,
+    name: string,
+    startDateTime: string,
+    endDateTime: string
+}
+
+export async function apiGetAllCalendarEvents(): Promise<CalendarEventModel[]> {
+    const eventsFromApi = [] as CalendarEventModel[];
+
+    await fetch(API_CALENDAR_EVENT_ROUTE)
+        .then(res => {
+            return res.json();
+        })
+        .then(body => {
+            for (const apiEvent of body as ApiEventResponse[]) {
+                const startDateTime = new Date(apiEvent.startDateTime);
+                const endDateTime = new Date(apiEvent.endDateTime);
+
+                eventsFromApi.push(
+                    new CalendarEventModel(
+                        apiEvent.name,
+                        startDateTime,
+                        endDateTime
+                    )
+                );
+            }
+        })
+        .catch(err => {
+            console.error(`Could not reach server; ${err}`);
+        });
+
+    return eventsFromApi;
+}
--- calendar-vue/src/stores/CalendarStore.ts
+++ calendar-vue/src/stores/CalendarStore.ts
@@ -1,11 +1,23 @@
 import { defineStore } from "pinia";
 
 import { CalendarEventModel } from "../models/CalendarEventModel.ts";
-import { apiGetAllCalendarEvents } from "../api/events.ts";
+import { apiGetAllCalendarEvents } from "../api/getAllEvents.ts";
 
 export const useEventStore = defineStore('events', {
     state: () => ({
-        events: getInitialEvents(),
+        events: [
+            new CalendarEventModel(
+                "wed 7 aug 24 test",
+                // This is 7th AUGUST because JavaScript is stupid.
+                new Date(2024, 7, 7, 10, 0, 0, 0),
+                new Date(2024, 7, 7, 12, 0, 0, 0)
+            ),
+            new CalendarEventModel(
+                "fri 2 aug 2024 test",
+                new Date(2024, 7, 2, 12, 0, 0, 0),
+                new Date(2024, 7, 2, 16, 0, 0, 0)
+            )
+        ] as CalendarEventModel[],
         selectedEvent: null as CalendarEventModel | null
     }),
     actions: {
@@ -48,22 +60,11 @@ export const useEventStore = defineStore('events', {
     }
 });
 
-function getInitialEvents(): CalendarEventModel[] {
-    const events = [
-        new CalendarEventModel(
-            "wed 7 aug 24 test",
-            // This is 7th AUGUST because JavaScript is stupid.
-            new Date(2024, 7, 7, 10, 0, 0, 0),
-            new Date(2024, 7, 7, 12, 0, 0, 0)
-        ),
-        new CalendarEventModel(
-            "fri 2 aug 2024 test",
-            new Date(2024, 7, 2, 12, 0, 0, 0),
-            new Date(2024, 7, 2, 16, 0, 0, 0)
-        )
-    ] as CalendarEventModel[];
+apiGetAllCalendarEvents()
+    .then(serverEvents => {
+        const eventStore = useEventStore();
 
-    apiGetAllCalendarEvents();
-
-    return events;
-}
+        serverEvents.forEach(event => {
+            eventStore.events.push(event);
+        });
+    })
